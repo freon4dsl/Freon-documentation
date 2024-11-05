27b5e4ad0724f49f792784a1f83d88909e66cd2b
Anneke Kleppe <anneke@warmerkleppe.nl>
2024-11-05 18:12:12 +0100

    Small changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,14 +30,14 @@ import { miniWindow } from '$lib';
 	<div class="landing-started">
 		<a href="/GettingStarted"
 			>Get Started
-			<div class="landing-arrow">
+			<span class="landing-arrow">
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height="28px"
 					><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
 						fill="currentColor"
 						d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"
 					/></svg
 				>
-			</div>
+			</span>
 		</a>
 	</div>
 </section>
