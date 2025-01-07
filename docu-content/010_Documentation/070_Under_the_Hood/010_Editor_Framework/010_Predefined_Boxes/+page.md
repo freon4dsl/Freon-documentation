59d652be5e853d2b95c8682675e384b3bf87d8e7
Anneke Kleppe <anneke@warmerkleppe.nl>
2025-01-07 13:08:30 +0100

    Fixed more todos in docu-content

--- docu-content/010_Documentation/070_Under_the_Hood/010_Editor_Framework/010_Predefined_Boxes/+page.md
+++ docu-content/010_Documentation/070_Under_the_Hood/010_Editor_Framework/010_Predefined_Boxes/+page.md
@@ -6,19 +6,19 @@ each box has a mandatory model node and role. The following predefined boxes are
 
 | Primitive Boxes   | Grouping Boxes      | Boxes for External Components |
 |-------------------|---------------------|-------------------------------|
-| AliasBox          | ActionBox           | ExternalBooleanBox            |
-| BooleanControlBox | FragmentBox         | ExternalNumberBox             |
-| ButtonBox         | GridBox             | ExternalPartBox               |
-| EmptyLineBox      | GridCellBox         | ExternalPartListBox           |
-| IndentBox         | HorizontalLayoutBox | ExternalRefBox                |
-| LabelBox          | HorizontalListBox   | ExternalRefListBox            |
-| NumberControlBox  | LayoutBox           | ExternalSimpleBox             |
-| LimitedControlBox | OptionalBox         | ExternalStringBox             |
-| MultiLineTextBox  | TableBox            | BooleanWrapperBox             |
-| SelectBox         | TableRowBox         | FragmentWrapperBox            |
-| SvgBox            | VerticalLayoutBox   | NumberWrapperBox              |
-| TextBox           | VerticalListBox     | PartWrapperBox                |
-|                   |                     | PartListWrapperBox            |
+| ActionBox         | FragmentBox         | ExternalBooleanBox            |
+| AliasBox          | GridBox             | ExternalNumberBox             |
+| BooleanControlBox | GridCellBox         | ExternalPartBox               |
+| ButtonBox         | HorizontalLayoutBox | ExternalPartListBox           |
+| EmptyLineBox      | HorizontalListBox   | ExternalRefBox                |
+| IndentBox         | OptionalBox         | ExternalRefListBox            |
+| LabelBox          | TableBox            | ExternalSimpleBox             |
+| NumberControlBox  | TableCellBox        | ExternalStringBox             |
+| LimitedControlBox | TableRowBox         | BooleanWrapperBox             |
+| MultiLineTextBox  | VerticalLayoutBox   | FragmentWrapperBox            |
+| SelectBox         | VerticalListBox     | NumberWrapperBox              |
+| SvgBox            |                     | PartWrapperBox                |
+| TextBox           |                     | PartListWrapperBox            |
 |                   |                     | RefWrapperBox                 |
 |                   |                     | RefListWrapperBox             |
 |                   |                     | StringWrapperBox              | 
@@ -26,12 +26,22 @@ each box has a mandatory model node and role. The following predefined boxes are
 All box types have a corresponding Component that renders the box in the browser. These components are build
 with <a href="https://svelte.dev" target="_blank">Svelte</a>. In the source code the box implementations reside in
 the package `core`, whereas the components reside in `core-svelte`.
+The Freon core package offers several utilities to create boxes, for instance in custom projections.
 
 The boxes for external components are explained in [External Component Box Types](/Documentation/Under_the_Hood/Editor_Framework/External_Component_Box_Types).
 
 ## Primitive Boxes
 
 Primitive boxes do not have other boxes as content. A primitive box is used to project a primitive AST node in the editor.
+
+### ActionBox
+
+An `ActionBox` is used to offer the user the option of executing an action, for instance the addition of an optional node.
+It has a placeholder text that is shown, and a dropdown menu that offers the various actions. Actions are coupled to the box based on the role.
+
+```ts
+new ActionBox(node, "optional-init", "init");
+```
 
 ### AliasBox
 
@@ -203,61 +213,126 @@ no longer any characters.
 Grouping or layout Boxes have other boxes as content. A grouping box is used to project a composed AST node, like
 a list, or a concept that has part properties, in the editor.
 
-### ActionBox
-
-[//]: # (todo)
-
 ### FragmentBox
+
+A `FragmentBox` is used to show a group of properties of a node. Its child boxes can be any type of box.
+
+```ts
+new FragmentBox(this._node, "BaseProduct-fragment-FirstCard", children);
+```
 
 ### GridBox
 
-A `GridBox` is used to show any child boxes in a tabular fashion.
-// TODO document GridBox
+A `GridBox` is used to show any child boxes in a tabular fashion, whereas a `TableBox` is used to show the elements from a list of similarly types nodes.
+All children of a `GridBox` should be of the type `GridCellBox`.
+
+```ts
+new GridBox(node, "grid-or", gridCells);
+```
 
 ### GridCellBox
 
-### HorizontalLayoutBox
-
-A `HorizontalLayoutBox` show a list of child boxes next to each other horizontally.
-
-An example of using a `HorizontalLayoutBox` is the following that represents the
-projection of a StringLiteral.
+A `GridCellBox` is used to show a single child box in a tabular fashion. It is always a child of a `GridBox`.
+It takes the row and column numbers within the grid as parameters.
 
 ```ts
-// TODO example
+new GridCellBox(fraction, "numerator", rowNr, columnNr, childBox);
+```
+
+### HorizontalLayoutBox
+
+A `HorizontalLayoutBox` show any child boxes next to each other horizontally, whereas a HorizontalListBox is used to show the elements from a list of similarly types nodes.
+
+An example of using a `HorizontalLayoutBox` is the following that represents the
+projection of a EuroLiteral with an icon in front of the actual value.
+
+```ts
+new HorizontalLayoutBox(node,
+        "EuroLiteral-hlist-line-0",
+        [
+           new SvgBox(node, "euro-icon", euroIcon, {
+              viewPortWidth: 20,
+              viewPortHeight: 20,
+              viewBoxWidth: 1024,
+              viewBoxHeight: 1024,
+              selectable: false
+           }),
+           BoxUtil.numberBox(node, "euros", NumberDisplay.SELECT),
+           BoxUtil.labelBox(node, ",", "top-1-line-0-item-2"),
+           BoxUtil.numberBox(node, "cents", NumberDisplay.SELECT),
+        ]);
 ```
 
 ### HorizontalListBox
 
-A `HorizontalListBox` shows all nodes from a list next to each other horizontally.
+A `HorizontalListBox` shows all members of one AST list property next to each other horizontally.
 
-### LayoutBox
+```ts
+new HorizontalListBox(
+        myMethodNode,
+		role,
+        "parameters", /* property name */
+        myMethodNode.parameters /* children */ )
+```
 
 ### OptionalBox
 
-An `OptionalBox` shows an optional projection.
+An `OptionalBox` shows an optional projection. The optional content is always present in the
+`childBox`. Next to the content there is an `ActionBox`, which is shown when the content is not
+present in the AST model.
+The parameters `mustShow` and `condition` determine which of the pair [content, placeholder] is shown. If the `condition`
+results in true, then the content box is shown. If `mustShow` is true, then the content box is also shown, even though
+there may not be actual content within the AST model.
 
-// TODO document OptionalBox
-
-There are three types of boxes for properties that are lists. It is assumed that all child boxes have the same type.
+```ts
+new OptionalBox(node, role, condition, contentBox, mustShow, actionBox);
+```
 
 ### TableBox
 
-A `TableBox` shows a list from the model in a table, either row-based or column-based.
+A `TableBox` shows a list from the model in a table, either row-based or column-based. A `TableBox` is an abstract class, which has two
+concrete subclasses: `TableBoxColumnOriented`, and `TableBoxRowOriented`. All children must be of type `TabelRowBox`. 
+
+```ts
+if (orientation === "column") {
+   return new TableBoxColumnOriented(node, listName, listElementType, roleName, hasHeaders, children);
+} else {
+   return new TableBoxRowOriented(node, listName, listElementType, roleName, hasHeaders, children);
+}
+```
+
+### TableCellBox
+
+A `TableCellBox` shows a property of one element of an AST list. The `rowNr` and `columnNr` give the position of the cell within the table.
+
+```ts
+new TableCellBox(
+        node,
+        propertyName,
+        propertyIndex,
+        propertyTypeName,
+        role,
+        rowNr,
+        columnNr,
+        contentBox
+);
+```
 
 ### TableRowBox
 
-A `TableBox` shows a list from the model in a table, either row-based or column-based.
+A `TableRowBox` shows one element of an AST list. This element may be spread out over multiple table cell boxes.
+The index gives the number of the row or column that the element is displayed in.
+
+```ts
+new TableRowBox(node, role, myCells, index);
+```
 
 ### VerticalLayoutBox
 
 A `VerticalLayoutBox` show a list of child boxes next to each other vertically.
-
-Its usage is similar to that of the `VerticalLayoutBox`.
+Its usage is similar to that of the `HorizontalLayoutBox`.
 
 ### VerticalListBox
 
-A `VerticalListBox` shows all nodes from a list one after another vertically.
-
-There are also boxes that can group any set of child boxes, no matter what there types are.
-
+A `VerticalListBox` shows all members of one AST list property one after another vertically.
+Its usage is similar to that of the `VerticalLayoutBox`.
--- src/lib/sidebar/SidebarContent.ts
+++ src/lib/sidebar/SidebarContent.ts
@@ -1,119 +1,144 @@
 import { type CategoryInfoType, type TocContentsType } from '$lib/sidebar/TocContentsType';
-		
-export const documentationToc: TocContentsType = 
-   { name: 'Documentation', path: '/Documentation',
-     content: [
-      { name: 'Overview', path: '/Documentation/Overview',
-        content: [
-         { name: 'Getting Started', path: '/Documentation/Overview/Getting_Started'},
-         { name: 'Running Example', path: '/Documentation/Overview/Running_Example'},
-       ]},
-      { name: 'Creating the Metamodel', path: '/Documentation/Creating_the_Metamodel',
-        content: [
-         { name: 'Language Structure', path: '/Documentation/Creating_the_Metamodel/Language_Structure'},
-         { name: 'Defining Properties', path: '/Documentation/Creating_the_Metamodel/Defining_Properties'},
-       ]},
-      { name: 'Defining an Editor', path: '/Documentation/Defining_an_Editor',
-        content: [
-         { name: 'Projections', path: '/Documentation/Defining_an_Editor/Projections'},
-         { name: 'Indentation', path: '/Documentation/Defining_an_Editor/Indentation'},
-         { name: 'Ease of Editing', path: '/Documentation/Defining_an_Editor/Ease_of_Editing'},
-         { name: 'Boolean Projections', path: '/Documentation/Defining_an_Editor/Boolean_Projections'},
-         { name: 'Number Projections', path: '/Documentation/Defining_an_Editor/Number_Projections'},
-         { name: 'Projections for Limited Concepts', path: '/Documentation/Defining_an_Editor/Projections_for_Limited_Concepts'},
-         { name: 'Binary Expressions', path: '/Documentation/Defining_an_Editor/Binary_Expressions'},
-         { name: 'Global Projections', path: '/Documentation/Defining_an_Editor/Global_Projections'},
-         { name: 'Buttons', path: '/Documentation/Defining_an_Editor/Buttons'},
-         { name: 'Fragments', path: '/Documentation/Defining_an_Editor/Fragments'},
-         { name: 'Styling', path: '/Documentation/Defining_an_Editor/Styling'},
-         { name: 'The Parser Projection', path: '/Documentation/Defining_an_Editor/The_Parser_Projection'},
-       ]},
-      { name: 'Scoping, Typing, and Validating', path: '/Documentation/Scoping,_Typing,_and_Validating',
-        content: [
-         { name: 'Scope Provider', path: '/Documentation/Scoping,_Typing,_and_Validating/Scope_Provider'},
-         { name: 'Type Provider', path: '/Documentation/Scoping,_Typing,_and_Validating/Type_Provider'},
-         { name: 'Validator', path: '/Documentation/Scoping,_Typing,_and_Validating/Validator'},
-       ]},
-      { name: 'Customizations', path: '/Documentation/Customizations',
-        content: [
-         { name: 'Editor Customization', path: '/Documentation/Customizations/Editor_Customization'},
-         { name: 'External Components', path: '/Documentation/Customizations/External_Components'},
-         { name: 'Scoper Customization', path: '/Documentation/Customizations/Scoper_Customization'},
-         { name: 'Typer Customization', path: '/Documentation/Customizations/Typer_Customization'},
-         { name: 'Validator Customization', path: '/Documentation/Customizations/Validator_Customization'},
-         { name: 'Other Customizations', path: '/Documentation/Customizations/Other_Customizations'},
-       ]},
-      { name: 'Under the Hood', path: '/Documentation/Under_the_Hood',
-        content: [
-         { name: 'Editor Framework', path: '/Documentation/Under_the_Hood/Editor_Framework'},
-         { name: 'Predefined Boxes', path: '/Documentation/Under_the_Hood/Editor_Framework/Predefined_Boxes'},
-         { name: 'External Component Box Types', path: '/Documentation/Under_the_Hood/Editor_Framework/External_Component_Box_Types'},
-         { name: 'Core Interfaces', path: '/Documentation/Under_the_Hood/Core_Interfaces'},
-         { name: 'Editor Interfaces', path: '/Documentation/Under_the_Hood/Editor_Interfaces'},
-         { name: 'FreTool Interfaces', path: '/Documentation/Under_the_Hood/FreTool_Interfaces'},
-       ]},
-      { name: 'Terminology', path: '/Documentation/Terminology'},
-    ]};
-export const tutorialToc: TocContentsType = 
-   { name: 'Tutorial', path: '/Tutorial',
-     content: [
-      { name: 'Overview', path: '/Tutorial/Overview'},
-      { name: 'Creating your Metamodel', path: '/Tutorial/Creating_your_Metamodel'},
-      { name: 'Making an Editor', path: '/Tutorial/Making_an_Editor'},
-      { name: 'More Fun with Projections', path: '/Tutorial/More_Fun_with_Projections'},
-      { name: 'Expressions in Freon', path: '/Tutorial/Expressions_in_Freon'},
-      { name: 'Projections for Expressions', path: '/Tutorial/Projections_for_Expressions'},
-      { name: 'In Need of Scoping', path: '/Tutorial/In_Need_of_Scoping'},
-      { name: 'More Scoping', path: '/Tutorial/More_Scoping'},
-      { name: 'How to Handle Typing', path: '/Tutorial/How_to_Handle_Typing'},
-      { name: 'Validating the model', path: '/Tutorial/Validating_the_model'},
-      { name: 'Conclusion', path: '/Tutorial/Conclusion'},
-    ]};
-export const examplesToc: TocContentsType = 
-   { name: 'Examples', path: '/Examples',
-     content: [
-      { name: 'Overview', path: '/Examples/Overview'},
-      { name: 'Projection Fragments', path: '/Examples/Projection_Fragments'},
-      { name: 'Custom Projections', path: '/Examples/Custom_Projections'},
-      { name: 'Editor Styling', path: '/Examples/Editor_Styling'},
-      { name: 'Custom Actions', path: '/Examples/Custom_Actions'},
-      { name: 'External Components', path: '/Examples/External_Components'},
-      { name: 'Generic Types', path: '/Examples/Generic_Types',
-        content: [
-         { name: 'Example Structure', path: '/Examples/Generic_Types/Example_Structure'},
-         { name: 'Example Typer Definition', path: '/Examples/Generic_Types/Example_Typer_Definition'},
-       ]},
-      { name: 'Using the Interpreter', path: '/Examples/Using_the_Interpreter'},
-      { name: 'Custom Scoping', path: '/Examples/Custom_Scoping'},
-      { name: 'Custom Typing', path: '/Examples/Custom_Typing'},
-      { name: 'Custom Validation', path: '/Examples/Custom_Validation'},
-      { name: 'Including Freon in Your Website', path: '/Examples/Including_Freon_in_Your_Website'},
-    ]};
-export const demoToc: TocContentsType = 
-   { name: 'Demo', path: '/Demo',
-     content: [
-      { name: 'Overview', path: '/Demo/Overview'},
-    ]};
-export const lionWebToc: TocContentsType = 
-   { name: 'LionWeb', path: '/LionWeb',
-     content: [
-      { name: 'Overview', path: '/LionWeb/Overview'},
-    ]};
-export const backgroundToc: TocContentsType = 
-   { name: 'Background', path: '/Background',
-     content: [
-      { name: 'Overview', path: '/Background/Overview'},
-      { name: 'Projectional Editing', path: '/Background/Projectional_Editing'},
-      { name: 'About the Name', path: '/Background/About_the_Name'},
-      { name: 'About the Authors', path: '/Background/About_the_Authors'},
-      { name: 'About the Site', path: '/Background/About_the_Site'},
-    ]};
-		
+
+export const documentationToc: TocContentsType = {
+	name: 'Documentation',
+	path: '/Documentation',
+	content: [
+		{
+			name: 'Overview',
+			path: '/Documentation/Overview',
+			content: [
+				{ name: 'Getting Started', path: '/Documentation/Overview/Getting_Started' },
+				{ name: 'Running Example', path: '/Documentation/Overview/Running_Example' }
+			]
+		},
+		{
+			name: 'Creating the Metamodel',
+			path: '/Documentation/Creating_the_Metamodel',
+			content: [
+				{ name: 'Language Structure', path: '/Documentation/Creating_the_Metamodel/Language_Structure' },
+				{ name: 'Defining Properties', path: '/Documentation/Creating_the_Metamodel/Defining_Properties' }
+			]
+		},
+		{
+			name: 'Defining an Editor',
+			path: '/Documentation/Defining_an_Editor',
+			content: [
+				{ name: 'Projections', path: '/Documentation/Defining_an_Editor/Projections' },
+				{ name: 'Indentation', path: '/Documentation/Defining_an_Editor/Indentation' },
+				{ name: 'Ease of Editing', path: '/Documentation/Defining_an_Editor/Ease_of_Editing' },
+				{ name: 'Boolean Projections', path: '/Documentation/Defining_an_Editor/Boolean_Projections' },
+				{ name: 'Number Projections', path: '/Documentation/Defining_an_Editor/Number_Projections' },
+				{ name: 'Projections for Limited Concepts', path: '/Documentation/Defining_an_Editor/Projections_for_Limited_Concepts' },
+				{ name: 'Binary Expressions', path: '/Documentation/Defining_an_Editor/Binary_Expressions' },
+				{ name: 'Global Projections', path: '/Documentation/Defining_an_Editor/Global_Projections' },
+				{ name: 'Buttons', path: '/Documentation/Defining_an_Editor/Buttons' },
+				{ name: 'Fragments', path: '/Documentation/Defining_an_Editor/Fragments' },
+				{ name: 'Styling', path: '/Documentation/Defining_an_Editor/Styling' },
+				{ name: 'The Parser Projection', path: '/Documentation/Defining_an_Editor/The_Parser_Projection' }
+			]
+		},
+		{
+			name: 'Scoping, Typing, and Validating',
+			path: '/Documentation/Scoping,_Typing,_and_Validating',
+			content: [
+				{ name: 'Scope Provider', path: '/Documentation/Scoping,_Typing,_and_Validating/Scope_Provider' },
+				{ name: 'Type Provider', path: '/Documentation/Scoping,_Typing,_and_Validating/Type_Provider' },
+				{ name: 'Validator', path: '/Documentation/Scoping,_Typing,_and_Validating/Validator' }
+			]
+		},
+		{
+			name: 'Customizations',
+			path: '/Documentation/Customizations',
+			content: [
+				{ name: 'Editor Customization', path: '/Documentation/Customizations/Editor_Customization' },
+				{ name: 'External Components', path: '/Documentation/Customizations/External_Components' },
+				{ name: 'Scoper Customization', path: '/Documentation/Customizations/Scoper_Customization' },
+				{ name: 'Typer Customization', path: '/Documentation/Customizations/Typer_Customization' },
+				{ name: 'Validator Customization', path: '/Documentation/Customizations/Validator_Customization' },
+				{ name: 'Other Customizations', path: '/Documentation/Customizations/Other_Customizations' }
+			]
+		},
+		{
+			name: 'Under the Hood',
+			path: '/Documentation/Under_the_Hood',
+			content: [
+				{ name: 'Editor Framework', path: '/Documentation/Under_the_Hood/Editor_Framework' },
+				{ name: 'Predefined Boxes', path: '/Documentation/Under_the_Hood/Editor_Framework/Predefined_Boxes' },
+				{ name: 'External Component Box Types', path: '/Documentation/Under_the_Hood/Editor_Framework/External_Component_Box_Types' },
+				{ name: 'Core Interfaces', path: '/Documentation/Under_the_Hood/Core_Interfaces' },
+				{ name: 'Editor Interfaces', path: '/Documentation/Under_the_Hood/Editor_Interfaces' },
+				{ name: 'FreTool Interfaces', path: '/Documentation/Under_the_Hood/FreTool_Interfaces' }
+			]
+		},
+		{ name: 'Terminology', path: '/Documentation/Terminology' }
+	]
+};
+export const tutorialToc: TocContentsType = {
+	name: 'Tutorial',
+	path: '/Tutorial',
+	content: [
+		{ name: 'Overview', path: '/Tutorial/Overview' },
+		{ name: 'Creating your Metamodel', path: '/Tutorial/Creating_your_Metamodel' },
+		{ name: 'Making an Editor', path: '/Tutorial/Making_an_Editor' },
+		{ name: 'More Fun with Projections', path: '/Tutorial/More_Fun_with_Projections' },
+		{ name: 'Expressions in Freon', path: '/Tutorial/Expressions_in_Freon' },
+		{ name: 'Projections for Expressions', path: '/Tutorial/Projections_for_Expressions' },
+		{ name: 'In Need of Scoping', path: '/Tutorial/In_Need_of_Scoping' },
+		{ name: 'More Scoping', path: '/Tutorial/More_Scoping' },
+		{ name: 'How to Handle Typing', path: '/Tutorial/How_to_Handle_Typing' },
+		{ name: 'Validating the model', path: '/Tutorial/Validating_the_model' },
+		{ name: 'Conclusion', path: '/Tutorial/Conclusion' }
+	]
+};
+export const examplesToc: TocContentsType = {
+	name: 'Examples',
+	path: '/Examples',
+	content: [
+		{ name: 'Overview', path: '/Examples/Overview' },
+		{ name: 'Projection Fragments', path: '/Examples/Projection_Fragments' },
+		{ name: 'Custom Projections', path: '/Examples/Custom_Projections' },
+		{ name: 'Editor Styling', path: '/Examples/Editor_Styling' },
+		{ name: 'Custom Actions', path: '/Examples/Custom_Actions' },
+		{ name: 'External Components', path: '/Examples/External_Components' },
+		{
+			name: 'Generic Types',
+			path: '/Examples/Generic_Types',
+			content: [
+				{ name: 'Example Structure', path: '/Examples/Generic_Types/Example_Structure' },
+				{ name: 'Example Typer Definition', path: '/Examples/Generic_Types/Example_Typer_Definition' }
+			]
+		},
+		{ name: 'Using the Interpreter', path: '/Examples/Using_the_Interpreter' },
+		{ name: 'Custom Scoping', path: '/Examples/Custom_Scoping' },
+		{ name: 'Custom Typing', path: '/Examples/Custom_Typing' },
+		{ name: 'Custom Validation', path: '/Examples/Custom_Validation' },
+		{ name: 'Including Freon in Your Website', path: '/Examples/Including_Freon_in_Your_Website' }
+	]
+};
+export const demoToc: TocContentsType = { name: 'Demo', path: '/Demo', content: [{ name: 'Overview', path: '/Demo/Overview' }] };
+export const lionWebToc: TocContentsType = {
+	name: 'LionWeb',
+	path: '/LionWeb',
+	content: [{ name: 'Overview', path: '/LionWeb/Overview' }]
+};
+export const backgroundToc: TocContentsType = {
+	name: 'Background',
+	path: '/Background',
+	content: [
+		{ name: 'Overview', path: '/Background/Overview' },
+		{ name: 'Projectional Editing', path: '/Background/Projectional_Editing' },
+		{ name: 'About the Name', path: '/Background/About_the_Name' },
+		{ name: 'About the Authors', path: '/Background/About_the_Authors' },
+		{ name: 'About the Site', path: '/Background/About_the_Site' }
+	]
+};
+
 export const allCategories: CategoryInfoType[] = [
-	{name: 'Documentation', path: '/Documentation', toc: documentationToc},
-		{name: 'Tutorial', path: '/Tutorial', toc: tutorialToc},
-		{name: 'Examples', path: '/Examples', toc: examplesToc},
-		{name: 'Demo', path: '/Demo', toc: demoToc},
-		{name: 'LionWeb', path: '/LionWeb', toc: lionWebToc},
-		{name: 'Background', path: '/Background', toc: backgroundToc}
+	{ name: 'Documentation', path: '/Documentation', toc: documentationToc },
+	{ name: 'Tutorial', path: '/Tutorial', toc: tutorialToc },
+	{ name: 'Examples', path: '/Examples', toc: examplesToc },
+	{ name: 'Demo', path: '/Demo', toc: demoToc },
+	{ name: 'LionWeb', path: '/LionWeb', toc: lionWebToc },
+	{ name: 'Background', path: '/Background', toc: backgroundToc }
 ];
