4c438a02229d4c094040cc69671c723ba542df98
Anneke Kleppe <anneke@warmerkleppe.nl>
2025-01-15 16:40:18 +0100

    Renamed folders

--- code-examples/CourseSchedule/phase2/PersonIcon.svelte
+++ code-examples/CourseSchedule/phase2/PersonIcon.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
     import {ExternalSimpleBox, FreEditor} from "@freon4dsl/core";
 
-    // Freon expects both of these to be present, even if they are not used.
+		// Declare the 'box' and 'editor' parameters as required by Freon
     export let box: ExternalSimpleBox;
     export let editor: FreEditor;
 
 </script>
 
+<!-- Display the icon with an image -->
 <img src='./icons8-person-94.png' alt="Icon showing Person" height="30px"/>
