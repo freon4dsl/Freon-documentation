3cba3af65b0d148b60760ceec86b262f437b1be2
Jos Warmer <jos.warmer@openmodeling.nl>
2025-01-23 12:29:03 +0100

    Build stuff

--- scripts/src/InstallCodeExamples.ts
+++ scripts/src/InstallCodeExamples.ts
@@ -4,9 +4,11 @@ import { execSync } from 'node:child_process';
  * Copy all examples from gothub project `freon-create-languages` to the code-examples folder.
  */
 {
-	for (const languageName of ["CourseSchedule", "CustomizationsProject", "Education", "Expressions", "Insurance"]) {
-		console.log(`Copying ${languageName}`)
-		const langRepo = `https://github.com/freon4dsl/create-freon-languages/languages/${languageName}`
-		await execSync(`npx degit -v ${langRepo}/src/ ../code-examples1/${languageName}/src/ --force`, { stdio: "inherit" })
+	// for (const languageName of ["CourseSchedule", "Education", "Expressions", "Insurance"]) {
+	for (const languageName of ['CourseSchedule', 'CustomizationsProject', 'Education', 'Expressions', 'Insurance']) {
+		console.log(`Copying ${languageName}`);
+		const langRepo = `https://github.com/freon4dsl/create-freon-languages/languages/${languageName}`;
+		await execSync(`npx degit -v ${langRepo}/src/ ../code-examples/${languageName}/src/ --force`, { stdio: 'inherit' });
+		await execSync(`npx degit -v ${langRepo}/extra/ ../code-examples/${languageName}/ --force`, { stdio: 'inherit' });
 	}
 }
