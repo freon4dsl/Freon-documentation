8139ca12bf2e15a13b263f902ace87e47dd5876f
Anneke Kleppe <anneke@warmerkleppe.nl>
2025-01-28 15:24:55 +0100

    Adjustments for installing the example projects from create-freon-languages

--- scripts/src/InstallCodeExamples.ts
+++ scripts/src/InstallCodeExamples.ts
@@ -4,7 +4,7 @@ import { execSync } from 'node:child_process';
  * Copy all examples from github project `create-freon-languages` to the code-examples folder.
  */
 {
-	for (const languageName of ['CourseSchedule', 'CustomizationsProject', 'Education', 'Expressions', 'Insurance', "TyperExample"]) {
+	for (const languageName of ['CourseSchedule', 'CustomizationsProject', 'Education', 'EducationInterpreter', 'Expressions', 'Insurance', "TyperExample"]) {
 		console.log(`Copying ${languageName}`);
 		const langRepo = `https://github.com/freon4dsl/create-freon-languages/languages/${languageName}`;
 		await execSync(`npx degit -v ${langRepo}/src/ ../code-examples/${languageName}/src/ --force`, { stdio: 'inherit' });
