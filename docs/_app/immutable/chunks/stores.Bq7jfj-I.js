7219c3d5591d1581dfc136b159a4db1131b1fdd7
Jos Warmer <jos.warmer@openmodeling.nl>
2025-02-18 15:14:51 +0100

    Add new docs

---
+++ docs/_app/immutable/chunks/stores.Bq7jfj-I.js
@@ -0,0 +1,2 @@
+import{s as e}from"./entry.CV5Hz3Wx.js";function c(s){return(s==null?void 0:s.length)!==void 0?s:Array.from(s)}const n=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},g={subscribe(s){return n().page.subscribe(s)}};export{c as e,g as p};
+//# sourceMappingURL=stores.Bq7jfj-I.js.map
